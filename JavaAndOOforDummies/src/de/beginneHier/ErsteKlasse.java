457e2afc6ff51d5a13cbe7c3fcd94a846206cd30
mhauser <mhauser@hsr.ch>
2013-08-17 12:00:23 +0200

    and so it starts...

---
+++ JavaAndOOforDummies/src/de/beginneHier/ErsteKlasse.java
@@ -0,0 +1,19 @@
+package de.beginneHier;
+
+/**
+ * Herzlich willkommen zu dieser kleinen Einführung zum Thema Java und
+ * Objektorientieres Programmieren (OO)!
+ * 
+ * @author Matthias
+ * @version 0.1
+ */
+public class ErsteKlasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
